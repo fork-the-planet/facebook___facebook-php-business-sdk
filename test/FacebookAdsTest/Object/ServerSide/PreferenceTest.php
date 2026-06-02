bfe140c9fea1b8288ec3c54d934412e564c9e8ff
Jiaming You <jiamingyou@meta.com>
2026-06-02 14:57:13 -0700

    Adding unit test files and e2e test files
    Summary: Adding unit test files and e2e test files
    
    Reviewed By: xuanjie22
    
    Differential Revision: D103490007
    
    fbshipit-source-id: 0c040dfc1c4589e722b02ab1fa4f9464b21bb3fe

---
+++ test/FacebookAdsTest/Object/ServerSide/PreferenceTest.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * You are hereby granted a non-exclusive, worldwide, royalty-free license to
+ * use, copy, modify, and distribute this software in source code or binary
+ * form for use in connection with the web services and APIs provided by
+ * Facebook.
+ *
+ * As with any software that integrates with the Facebook platform, your use
+ * of this software is subject to the Facebook Developer Principles and
+ * Policies [http://developers.facebook.com/policy/]. This copyright notice
+ * shall be included in all copies or substantial portions of the software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+namespace FacebookAdsTest\Object\ServerSide;
+
+use FacebookAdsTest\AbstractUnitTestCase;
+use FacebookAds\Object\ServerSide\Preference;
+
+class PreferenceTest extends AbstractUnitTestCase {
+
+  public function testDefaultsAllAllowed() {
+    $p = new Preference();
+    $this->assertTrue($p->isFbcAllowed());
+    $this->assertTrue($p->isFbpAllowed());
+    $this->assertTrue($p->isClientIpAddressAllowed());
+    $this->assertTrue($p->isReferrerUrlAllowed());
+  }
+
+  public function testAllDisallowed() {
+    $p = new Preference(false, false, false, false);
+    $this->assertFalse($p->isFbcAllowed());
+    $this->assertFalse($p->isFbpAllowed());
+    $this->assertFalse($p->isClientIpAddressAllowed());
+    $this->assertFalse($p->isReferrerUrlAllowed());
+  }
+
+  public function testPartialAllowlist() {
+    // Only fbc and client_ip_address allowed.
+    $p = new Preference(true, false, true, false);
+    $this->assertTrue($p->isFbcAllowed());
+    $this->assertFalse($p->isFbpAllowed());
+    $this->assertTrue($p->isClientIpAddressAllowed());
+    $this->assertFalse($p->isReferrerUrlAllowed());
+  }
+
+  public function testEachFlagIndependently() {
+    $cases = [
+      [true,  false, false, false, 'fbc'],
+      [false, true,  false, false, 'fbp'],
+      [false, false, true,  false, 'client_ip_address'],
+      [false, false, false, true,  'referrer_url'],
+    ];
+    foreach ($cases as $c) {
+      $p = new Preference($c[0], $c[1], $c[2], $c[3]);
+      $this->assertEquals($c[0], $p->isFbcAllowed(),               "fbc for $c[4]");
+      $this->assertEquals($c[1], $p->isFbpAllowed(),               "fbp for $c[4]");
+      $this->assertEquals($c[2], $p->isClientIpAddressAllowed(),   "ip for $c[4]");
+      $this->assertEquals($c[3], $p->isReferrerUrlAllowed(),       "referrer for $c[4]");
+    }
+  }
+}
